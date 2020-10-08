38820fcf6b7147a52559a2e0728b7d1675e8f0b7
unknown <metgudmath.ashwini7@gmail.com>
2020-10-08 19:39:17 +0200

    added another check

--- src/triangular.js
+++ src/triangular.js
@@ -1,6 +1,6 @@
 exports.findTriangleType = (a, b, c) => {
-  if (a <= 0 || b <= 0 || c <= 0) return `Cannot enter negative or zero values`;
   if (!a || !b || !c) return `Enter input values`;
+  else if (a <= 0 || b <= 0 || c <=0 ) return `Cannot enter negative or zero values`;
   else if (a + b < c || b + c < a || c + a < b) return `Cannot form a triangle`;
   else if (a === b && b === c) return `Equilateral triangle`;
   else if (a === b || b === c || c === a) return `Isosceles triangle`;
