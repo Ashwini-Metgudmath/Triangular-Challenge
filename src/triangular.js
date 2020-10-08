50a8a5da5ceba4338e8c6a247f673a6c487df021
unknown <metgudmath.ashwini7@gmail.com>
2020-10-08 19:16:31 +0200

    added another test case

--- src/triangular.js
+++ src/triangular.js
@@ -1,8 +1,8 @@
 exports.findTriangleType = (a, b, c) => {
-  if (!a || !b || !c) return `Enter positive, valid length for sides`;
+  if (a <= 0 || b <= 0 || c <= 0) return `Cannot enter negative or zero values`;
+  if (!a || !b || !c) return `Enter input values`;
   else if (a + b < c || b + c < a || c + a < b) return `Cannot form a triangle`;
   else if (a === b && b === c) return `Equilateral triangle`;
   else if (a === b || b === c || c === a) return `Isosceles triangle`;
   else return `Scalence triangle`;
 };
-
--- tests/triangular.test.js
+++ tests/triangular.test.js
@@ -3,20 +3,24 @@ const chai = require("chai");
 const expect = chai.expect;
 
 describe("Triangular tests", () => {
-    it("return invalid input", () => {
-        expect(findTriangleType()).to.be.equal("Enter positive, valid length for sides");
-    });
-    it("return not a triangle", () => {
-        expect(findTriangleType(2, 5, 1)).to.be.equal("Cannot form a triangle");
-    });
-    it("return equilateral type", () => {
-        expect(findTriangleType(3, 3, 3)).to.be.equal("Equilateral triangle");
-    });
-    it("return Isosceles type", () => {
-        expect(findTriangleType(3, 3, 6)).to.be.equal("Isosceles triangle");
-    });
-    it("return Scalence type", () => {
-        expect(findTriangleType(1, 2, 3)).to.be.equal("Scalence triangle");
-    });
-    
+  it("returns invalid for negative input", () => {
+    expect(findTriangleType(-1, -2, -3)).to.be.equal(
+      "Cannot enter negative or zero values"
+    );
+  });
+  it("returns invalid for zero input", () => {
+    expect(findTriangleType()).to.be.equal("Enter input values");
+  });
+  it("returns not a triangle", () => {
+    expect(findTriangleType(2, 5, 1)).to.be.equal("Cannot form a triangle");
+  });
+  it("returns equilateral type", () => {
+    expect(findTriangleType(3, 3, 3)).to.be.equal("Equilateral triangle");
+  });
+  it("returns Isosceles type", () => {
+    expect(findTriangleType(3, 3, 6)).to.be.equal("Isosceles triangle");
+  });
+  it("returns Scalence type", () => {
+    expect(findTriangleType(1, 2, 3)).to.be.equal("Scalence triangle");
+  });
 });
